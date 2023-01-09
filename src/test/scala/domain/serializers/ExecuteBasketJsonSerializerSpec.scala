2dcbe301685a21bad2ba3c1682501a734e6e6481
Ezequiel Borenstein <ezequielborenstein@MacBook-Pro.fibertel.com.ar>
2023-01-08 22:06:35 -0300

    Run test for 10 random cases of command

--- src/test/scala/domain/serializers/ExecuteBasketJsonSerializerSpec.scala
+++ src/test/scala/domain/serializers/ExecuteBasketJsonSerializerSpec.scala
@@ -9,21 +9,20 @@ import org.scalatest.matchers.*
 import org.scalatest.wordspec.AnyWordSpecLike
 
 class ExecuteBasketJsonSerializerSpec extends AnyWordSpecLike with should.Matchers {
-  val randomPopulation = BasketGenerator.generateRandomPopulation(10)
   val serializer = new ExecuteBasketJsonSerializer
 
   "Serializer" should {
     "serialize and deserialize a command for the empty population" in {
       val expectedCommand = Execute(EVOLUTION, List())
       val currentCommand = serializer.read(serializer.write(expectedCommand))
-      currentCommand should be (currentCommand)
+      currentCommand should be(currentCommand)
     }
 
     "serialize and deserialize a command for a random population" in {
-      val expectedCommand = Execute(EVOLUTION, randomPopulation)
-      val currentCommand = serializer.read(serializer.write(expectedCommand))
-      currentCommand should be (currentCommand)
+      val commands = (1 to 10).map{_ =>
+        Execute(EVOLUTION, BasketGenerator.generateRandomPopulation(10))
+      }
+      commands.foreach(expectedCommand => expectedCommand should be(serializer.read(serializer.write(expectedCommand))))
     }
   }
-
 }
