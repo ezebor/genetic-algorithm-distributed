12d49b58fd0038d0fffdb821a8d7e65914fb5617
Ezequiel Borenstein <ezequiel.borenstein@gmail.com>
2023-03-30 14:54:52 -0300

    Move intermediate receivers inside the god receiver

--- src/main/scala/domain/actors/EvolutionMaster.scala
+++ src/main/scala/domain/actors/EvolutionMaster.scala
@@ -25,34 +25,35 @@ class EvolutionMaster(quantityOfWorkers: Int, router: ActorRef, originalSender: 
     case HEALTH => sender() ! OK
   }
 
-  private def online: Receive = {
-    case Execute(EVOLUTION, population: Population) =>
-      self ! Execute(NATURAL_SELECTION, population)
-    case Execute(STOP, population: Population) =>
-      originalSender ! GenerationBuilt(population)
-    case Execute(currentOperatorName: String, population: Population) =>
-      val (basePopulation: Population, nextOperatorName: String) = currentOperatorName match
-        case NATURAL_SELECTION => (Population(List()), CROSSOVER)
-        case CROSSOVER => (population, MUTATION)
-        case MUTATION => (population, STOP)
+  private def online: Receive = { message =>
+    def waitingWorkers(nextOperatorName: String)(evolvedPopulation: Population, pendingWorkers: Int): Receive = {
+      case Execute(ADD_POPULATION, newPopulation: Population) =>
+        val finalPopulation = Population(evolvedPopulation.individuals ::: newPopulation.individuals)
+        if (pendingWorkers == 1) {
+          context.become(online)
+          self ! Execute(nextOperatorName, finalPopulation)
+        } else {
+          context.become(waitingWorkers(nextOperatorName)(finalPopulation, pendingWorkers - 1))
+        }
+    }
 
-      log.debug(s"Executing $currentOperatorName for a population with size = ${population.individuals.size}. Next operator: $nextOperatorName")
+    message match
+      case Execute(EVOLUTION, population: Population) =>
+        self ! Execute(NATURAL_SELECTION, population)
+      case Execute(STOP, population: Population) =>
+        originalSender ! GenerationBuilt(population)
+      case Execute(currentOperatorName: String, population: Population) =>
+        val (basePopulation: Population, nextOperatorName: String) = currentOperatorName match
+          case NATURAL_SELECTION => (Population(List()), CROSSOVER)
+          case CROSSOVER => (population, MUTATION)
+          case MUTATION => (population, STOP)
 
-      val chunks: List[Population] = population.intoChunks(population.individuals.size / quantityOfWorkers)
-      context.become(waitingWorkers(nextOperatorName)(basePopulation, chunks.size))
-      chunks.foreach(chunk => router ! Execute(currentOperatorName, chunk))
-    case HEALTH => sender() ! OK
-    case OFFLINE => context.become(offline)
-  }
+        log.debug(s"Executing $currentOperatorName for a population with size = ${population.individuals.size}. Next operator: $nextOperatorName")
 
-  private def waitingWorkers(nextOperatorName: String)(evolvedPopulation: Population, pendingWorkers: Int): Receive = {
-    case Execute(ADD_POPULATION, newPopulation: Population) =>
-      val finalPopulation = Population(evolvedPopulation.individuals ::: newPopulation.individuals)
-      if (pendingWorkers == 1) {
-        context.become(online)
-        self ! Execute(nextOperatorName, finalPopulation)
-      } else {
-        context.become(waitingWorkers(nextOperatorName)(finalPopulation, pendingWorkers - 1))
-      }
+        val chunks: List[Population] = population.intoChunks(population.individuals.size / quantityOfWorkers)
+        context.become(waitingWorkers(nextOperatorName)(basePopulation, chunks.size))
+        chunks.foreach(chunk => router ! Execute(currentOperatorName, chunk))
+      case HEALTH => sender() ! OK
+      case OFFLINE => context.become(offline)
   }
 }
--- src/main/scala/domain/actors/GenerationsManager.scala
+++ src/main/scala/domain/actors/GenerationsManager.scala
@@ -23,39 +23,39 @@ class GenerationsManager(
       context.become(firstOnline(evolutionMaster))
   }
 
-  private def firstOnline(evolutionMaster: ActorRef): Receive = {
-    case BuildNewGeneration(population: Population) =>
-      log.info(s"Starting generation [1]")
-      evolutionMaster ! Execute(EVOLUTION, population: Population)
-    case GenerationBuilt(population: Population) =>
-      context.become(steadyOnline(2, 0, Population(List(population.bestIndividual)), evolutionMaster))
-      self ! BuildNewGeneration(population)
-  }
-
-  private def steadyOnline(
-                            generationId: Int,
-                            quantityOfGenerationsWithoutImprovements: Int,
-                            solutions: Population,
-                            evolutionMaster: ActorRef): Receive = {
-    case BuildNewGeneration(population: Population) =>
-      log.info(s"Starting generation [$generationId] over a population with ${population.individuals.size} individuals")
-      evolutionMaster ! Execute(EVOLUTION, population)
-    case GenerationBuilt(population: Population) =>
-      if(population.bestIndividual.fitness.getOrElse(0d) > solutions.individuals.head.fitness.getOrElse(0d))
-        log.info(s"New better individual was found with fitness = ${population.bestIndividual.fitness}")
-        val newSolutions = Population(population.bestIndividual :: {
-          if(solutions.individuals.size == solutionsPopulationSize) solutions.individuals.dropRight(1)
-          else solutions.individuals
-        })
-        context.become(steadyOnline(generationId + 1, 0, newSolutions, evolutionMaster))
-        self ! BuildNewGeneration(population)
-      else
-        if(quantityOfGenerationsWithoutImprovements <= maxQuantityOfGenerationsWithoutImprovements)
-          context.become(steadyOnline(generationId + 1, quantityOfGenerationsWithoutImprovements + 1, solutions, evolutionMaster))
+  private def firstOnline(evolutionMaster: ActorRef): Receive = { message =>
+    def steadyOnline(
+                              generationId: Int,
+                              quantityOfGenerationsWithoutImprovements: Int,
+                              solutions: Population): Receive = {
+      case BuildNewGeneration(population: Population) =>
+        log.info(s"Starting generation [$generationId] over a population with ${population.individuals.size} individuals")
+        evolutionMaster ! Execute(EVOLUTION, population)
+      case GenerationBuilt(population: Population) =>
+        if(population.bestIndividual.fitness.getOrElse(0d) > solutions.individuals.head.fitness.getOrElse(0d))
+          log.info(s"New better individual was found with fitness = ${population.bestIndividual.fitness}")
+          val newSolutions = Population(population.bestIndividual :: {
+            if(solutions.individuals.size == solutionsPopulationSize) solutions.individuals.dropRight(1)
+            else solutions.individuals
+          })
+          context.become(steadyOnline(generationId + 1, 0, newSolutions))
           self ! BuildNewGeneration(population)
         else
-          evolutionMaster ! OFFLINE
-          log.info(s"Found solutions: $solutions")
-          context.become(offline)
+          if(quantityOfGenerationsWithoutImprovements <= maxQuantityOfGenerationsWithoutImprovements)
+            context.become(steadyOnline(generationId + 1, quantityOfGenerationsWithoutImprovements + 1, solutions))
+            self ! BuildNewGeneration(population)
+          else
+            evolutionMaster ! OFFLINE
+            log.info(s"Found solutions: $solutions")
+            context.become(offline)
+    }
+
+    message match
+      case BuildNewGeneration(population: Population) =>
+        log.info(s"Starting generation [1]")
+        evolutionMaster ! Execute(EVOLUTION, population: Population)
+      case GenerationBuilt(population: Population) =>
+        context.become(steadyOnline(2, 0, Population(List(population.bestIndividual))))
+        self ! BuildNewGeneration(population)
   }
 }
