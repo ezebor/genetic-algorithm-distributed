6b6e517557ed2cbf093fe9b6f6c1a91654b27c5c
Ezequiel Borenstein <ezequiel.borenstein@gmail.com>
2023-03-31 11:16:45 -0300

    todo

--- src/main/scala/app/ExecutionScript.scala
+++ src/main/scala/app/ExecutionScript.scala
@@ -1,5 +1,6 @@
 package app
 
+// TODO: parametrizar acá la población inicial
 object WorkersNode1 extends EvolutionWorkersNode(2561)
 object WorkersNode2 extends EvolutionWorkersNode(2562)
 object MasterNode extends EvolutionMasterNode(3)
