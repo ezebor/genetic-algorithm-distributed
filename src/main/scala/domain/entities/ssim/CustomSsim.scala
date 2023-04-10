c25dd1743ff73748366b4197045c9ec798b7db5e
Ezequiel Borenstein <ezequielborenstein@MacBook-Pro.fibertel.com.ar>
2023-04-10 19:30:51 -0300

    Read/write images

--- src/main/scala/domain/entities/ssim/CustomSsim.scala
+++ src/main/scala/domain/entities/ssim/CustomSsim.scala
@@ -1,7 +1,17 @@
 package domain.entities.ssim
 
-import com.sksamuel.scrimage._
+import com.sksamuel.scrimage.*
+import com.sksamuel.scrimage.filter.BlurFilter
+import com.sksamuel.scrimage.nio.PngWriter
+import com.sksamuel.scrimage.transform.BackgroundGradient
 
-object CustomSsim {
+import java.awt.Color
 
+object CustomSsim extends App {
+  val reference = ImmutableImage.loader().fromFile("src/main/scala/resources/ssim/cyndaquil.png")
+  val other = ImmutableImage.loader().fromFile("src/main/scala/resources/ssim/fusiongrass.png")
+
+  val transformed = reference.fill(Color.BLUE)
+
+  transformed.output(PngWriter.NoCompression, "src/main/scala/resources/ssim/cyndaquil2.png")
 }
