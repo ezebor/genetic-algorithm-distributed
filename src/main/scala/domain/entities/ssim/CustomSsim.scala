ef98f350d54b1f02200160492d3b28d76700aeb5
Ezequiel Borenstein <ezequiel.borenstein@gmail.com>
2023-04-12 18:42:13 -0300

    Completed the SSIM formula

--- src/main/scala/domain/entities/ssim/CustomSsim.scala
+++ src/main/scala/domain/entities/ssim/CustomSsim.scala
@@ -8,31 +8,26 @@ import com.sksamuel.scrimage.pixels.Pixel
 import com.sksamuel.scrimage.transform.BackgroundGradient
 
 import java.awt.Color
+import scala.util.Random
 
 object CustomSsim extends App {
-  val reference = ImmutableImage.loader().fromFile("src/main/scala/resources/ssim/cyndaquil.png")
-  val other = ImmutableImage.loader().fromFile("src/main/scala/resources/ssim/fusiongrass.png")
+  val reference = ImmutableImage.loader().fromFile("src/main/scala/resources/ssim/bulbasaur.png")
 
   // TODO: crear nueva imagen a partir de píxeles
   // TODO: IMPORTANTE ---> cuando haga crossover, si el pixel que quiero usar está ocupado buscar el primero libre
   // TODO: argb del pixel libre = -16777216
-  println(List.from(other.blank().pixels()))
+  //println(List.from(other.blank().pixels()))
   val newImage = reference.copy()
   val pixels = reference.pixels().map(pixel => Pixel(pixel.x, pixel.y, pixel.argb))
   pixels.foreach(pixel => newImage.setPixel(pixel))
-  newImage.output(PngWriter.MinCompression, "src/main/scala/resources/ssim/cyndaquil2.png")
+  //newImage.output(PngWriter.MinCompression, "src/main/scala/resources/ssim/cyndaquil2.png")
 
   // TODO: FORMULA
-  println(mu(reference))
-  println(mu(newImage))
-  println(mu(other))
-
-  println(sigma(reference))
-  println(sigma(newImage))
-  println(sigma(other))
+  println(ssim(reference, ImmutableImage.loader().fromFile("src/main/scala/resources/ssim/fusiongrass.png")))
+  println(ssim(reference, ImmutableImage.loader().fromFile("src/main/scala/resources/ssim/fusionfire.png")))
+  println(ssim(reference, newImage))
 
   // TODO: gaussian formula distribution: https://en.wikipedia.org/wiki/Normal_distribution
-
 
   // TODO: hacerlo mediante ventanas
   def mu(image: ImmutableImage): Double = List.from(image.pixels().map(_.average())).sum / image.count()
@@ -43,4 +38,28 @@ object CustomSsim extends App {
     Math.sqrt(List.from(image.pixels().map(pixel => Math.pow(pixel.average() - imageMu, 2))).sum / (image.count() - 1))
   }
 
+  def sigma(ref: ImmutableImage, comp: ImmutableImage): Double = {
+    val refMu = mu(ref)
+    val compMu = mu(comp)
+
+    val refAverages = ref.pixels().map(_.average())
+    val compAverages = comp.pixels().map(_.average())
+
+    val averagesSum = refAverages.indices.map(index => (refAverages(index) - refMu) * (compAverages(index) - compMu)).sum
+
+    averagesSum / (ref.count() - 1)
+  }
+
+  val k1 = 0.01
+  val k2 = 0.03f
+  val L = 255
+  val c1 = Math.pow(k1 * L, 2)
+  val c2 = Math.pow(k2 * L, 2)
+  val c3 = c2 / 2
+
+  def luminance(ref: ImmutableImage, comp: ImmutableImage) = (2 * mu(ref) * mu(comp) + c1) / (Math.pow(mu(ref), 2) + Math.pow(mu(comp), 2) + c1)
+  def contrast(ref: ImmutableImage, comp: ImmutableImage) = (2 * sigma(ref) * sigma(comp) + c2) / (Math.pow(sigma(ref), 2) + Math.pow(sigma(comp), 2) + c2)
+  def structure(ref: ImmutableImage, comp: ImmutableImage) = (sigma(ref, comp) + c3) / (sigma(ref) * sigma(comp) + c3)
+
+  def ssim(ref: ImmutableImage, comp: ImmutableImage) = luminance(ref, comp) * contrast(ref, comp) * structure(ref, comp)
 }
