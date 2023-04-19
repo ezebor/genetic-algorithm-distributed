ab3e0fc5e1811dea4e5fe80e45eed5bdee78e1e5
Ezequiel Borenstein <ezequiel.borenstein@gmail.com>
2023-04-19 18:01:52 -0300

    Fixed printer

--- src/main/scala/domain/actors/SolutionsPrinter.scala
+++ src/main/scala/domain/actors/SolutionsPrinter.scala
@@ -1,7 +1,11 @@
 package domain.actors
 
 import akka.actor.*
-import domain.entities.Population
+import com.sksamuel.scrimage.ImmutableImage
+import com.sksamuel.scrimage.nio.PngWriter
+import domain.entities.{Block, Frame, Image, Population}
+
+import scala.util.Success
 
 object SolutionsPrinter {
   def props(): Props = Props(new SolutionsPrinter())
@@ -9,6 +13,16 @@ object SolutionsPrinter {
 
 class SolutionsPrinter extends Actor with ActorLogging {
   override def receive: Receive = {
+    case Population(images: List[Image]) =>
+      images
+        .zipWithIndex
+        .map { case (image, index) =>
+          val newImage = ImmutableImage.create(500, 500)
+          image.frame match
+            case Success(Frame(blocks)) =>
+              blocks.flatMap(_.pixels).foreach(pixel => newImage.setPixel(pixel))
+              newImage.output(PngWriter.NoCompression, s"src/main/scala/resources/ssim/result_$index.png")
+        }
     case solutions: Population =>
       log.info(s"Found solutions: $solutions")
   }
