e1555d15ef1cb4c78294bc9d82c8b20713fdb942
Ezequiel Borenstein <ezequiel.borenstein@gmail.com>
2023-07-02 14:44:52 -0300

    Delete call to printer

--- src/main/scala/domain/actors/GenerationsManager.scala
+++ src/main/scala/domain/actors/GenerationsManager.scala
@@ -25,7 +25,6 @@ class GenerationsManager() extends BaseActor {
               if (solutions.individuals.size == solutionsPopulationSize) solutions.individuals.dropRight(1)
               else solutions.individuals
             })
-            printSolutions(population.copyWith(List(population.bestIndividual)))
             manageBuiltGeneration(generationId + 1, 0, newSolutions)
           }
           else if (quantityOfGenerationsWithoutImprovements < maxQuantityOfGenerationsWithoutImprovements)
